8179d753f02c2fd22cb0a955c92117a2504156ea
Carlos de Leon <admin@koxland.net>
2025-07-02 14:41:12 -0600

    Add i18n support for section titles and footer
    Refactored AboutUsContainer, WidgetsContainer, and Footer components to use react-i18next for section titles and copyright text. Updated en.json and es.json language files to include new translation keys for these sections.

--- src/components/containers/AboutUsContainer.jsx
+++ src/components/containers/AboutUsContainer.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { useTranslation } from "react-i18next";
 
 function AboutUsContainer() {
+  const { i18n, t } = useTranslation();
+
   return (
     <div className="w-full bg-[#297da9] p-5">
-      <h2 className="text-center text-[40px] font-bold text-white">About Us</h2>
+      <h2 className="text-center text-[40px] font-bold text-white">
+        {t("aboutUs.title")}
+      </h2>
       <p className="text-center text-white">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt
         reiciendis optio quas sed ipsum asperiores sapiente doloribus
