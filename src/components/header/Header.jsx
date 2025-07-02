211c64fc4ef33682e5b25c379fe5b22445f48b1a
Carlos de Leon <admin@koxland.net>
2025-07-02 14:48:06 -0600

    Update header layout and improve accessibility
    Adjusted the header grid layout for better alignment, updated button and heading styles, and added an accessible alt text to the logo image.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,17 +6,19 @@ function Header() {
   const { t } = useTranslation();
 
   return (
-    <header className="grid w-full grid-cols-[auto_1fr_auto] items-center justify-between p-5">
-      <button className="max-w-[48px] cursor-pointer rounded-full">
+    <header className="grid w-full grid-cols-[1fr_auto_1fr] items-center p-5">
+      <button className="mr-auto max-w-[48px] cursor-pointer rounded-full">
         <img
           className="rounded-full object-contain"
           src="/assets/logo.png"
-          alt=""
+          alt="Sacbe Logo Icon"
         />
       </button>
-      <h1 className="text-center text-3xl font-bold">Cancun Tours</h1>
-      <div className="flex gap-4">
-        <button className="btn-pulse w-[200px] cursor-pointer rounded-4xl bg-[#297da9] px-4 py-2 text-xl text-white transition-all duration-300 ease-in-out hover:bg-[#004165]">
+      <h1 className="w-fit justify-self-center text-5xl font-bold">
+        Cancun Tours
+      </h1>
+      <div className="flex w-fit items-center gap-4 justify-self-end">
+        <button className="btn-pulse w-[168px] cursor-pointer rounded-4xl bg-[#297da9] px-4 py-2 text-xl text-white transition-all duration-300 ease-in-out hover:bg-[#004165]">
           {t("header.bookButton")}
         </button>
         <LanguageButton />
